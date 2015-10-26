b19278153e6f507ff5b137c3a501a5c53b0bceb4
KaseKreative <KaseKreative@fullsail.edu>
2015-10-26 19:14:59 -0400

    boilerplate setup

---
+++ app.js
@@ -0,0 +1,33 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express 	= require('express'),
+ 	http 		= require('http'),
+ 	exphbs 		= require('express-handlebars')
+	path = require('path');
+
+var app = express();
+
+// all environments
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
