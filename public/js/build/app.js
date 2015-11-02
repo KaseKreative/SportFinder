a7acfa1d2eb08dc7ca5e20e62502b989c8f69cea
KaseKreative <KaseKreative@fullsail.edu>
2015-11-01 23:58:09 -0500

    working redirect after auth

--- public/js/build/app.js
+++ public/js/build/app.js
@@ -8,59 +8,106 @@ var app = angular.module('app', ['ngRoute', 'firebase'])
             }).when("/userProfile", {
             	templateUrl: "templates/profile.html",
             	controller: "profileController"
+            }).when("/TorT",{
+            	templateUrl: "templates/TorT.html",
+            	controller:  "TorTController"
+            }).when("/searchDashboard", {
+            	templateUrl: "templates/searchDashboard.html",
+            	controller: "searchDashboardController"
             }).otherwise({
                 redirectTo: "/"
             })
-	   }]);
+	}]);
 
 
-	app.controller('indexController', ['$scope', '$firebaseAuth', '$firebaseArray', '$rootScope', '$routeParams','$location', function($scope, $firebaseAuth, $firebaseArray, $rootScope, $routeParams, $location){
+	app.controller('indexController', ['$scope', '$firebaseAuth', '$firebaseObject', '$rootScope', '$routeParams','$location', function($scope, $firebaseAuth, $firebaseObject, $rootScope, $routeParams, $location){
 			 $scope.data = {};
 			 var ref = new Firebase("https://thesportfinder.firebaseio.com/users");
 			 $scope.authObj = $firebaseAuth(ref);
-
-
 			 
+
+
+
+			 console.log('bloop');
 			 $scope.facebook = function(){
-			 	$scope.authObj.$authWithOAuthPopup('facebook').then(function(authData){
-			 		
-			 		ref = new Firebase("https://thesportfinder.firebaseio.com/users/"+authData.uid);
-			 		$scope.users = $firebaseArray(ref);
-			 		var key = $scope.users.key();
-			 		console.log('key:', key);
-			 		var item = $scope.users.$getRecord(authData.uid);
-			 		// console.log($scope.users.$id);
-			 		console.log(item);
-			 		authData.facebook.uid = authData.uid;
-			 		$scope.users.$add(authData.facebook);
+			 		console.log('bloop2');
+			 		$scope.authObj.$onAuth(function(authData){
+			 			if(authData){
+			 			var reff = new Firebase("https://thesportfinder.firebaseio.com/users/"+authData.uid);
+			 			var reffObject = $firebaseObject(reff);
+			 			console.log(reffObject);
+			 				reffObject.$loaded().then(function(data){
+			 					console.log(data);
+							if(data.uid == undefined){
+								authData.facebook.uid = authData.uid;
+			 				var userData = authData.facebook.cachedUserProfile;
+			 					reff.set({
+			 					facebookID : userData.id,
+			 					firstName  : userData.first_name,
+			 					lastName   : userData.last_name,
+			 					name       : userData.name,
+			 					gender     : userData.gender,
+			 					link       : userData.link,
+			 					imgUrl     : userData.picture.data.url,
+			 					uid        : authData.uid
+			 					});
+			 					$location.path('/TorT')
+			 				}
+							}).catch(function(err){console.log('Error :', err);})
+			 				
 			 		$rootScope.user = authData.facebook;
-			 		$location.path('/userProfile');
-			 		console.log(authData);
-			 		console.log($scope.user);
-			 	})
+			 		$location.path('/searchDashboard');
+			 			}	
+			 		})
 			 };
 
 	}]);
 
 
 
-	app.controller('profileController', ['$scope', '$rootScope', '$routeParams','$location', function($scope, $rootScope, $routeParams, $location){ 
+	app.controller('profileController', ['$scope', '$rootScope', '$firebaseObject', '$routeParams','$location', function($scope, $rootScope, $firebaseObject, $routeParams, $location){ 
  			if(!$scope.user){
  				$location.path('/');
- 			}
- 			console.log($scope.user);
- 			// itemArray.$bindTo($scope, "data");
-			 // $scope.addItem = function(){
-			 // itemObject.item2= {'itemName': 'The Name 2'};
-			 // itemObject.$save();
-			 // console.log(itemObject);
-			 	// itemArray.$add({
-			 	// 		random : $scope.data.text
-			 	// });
+ 			}else{
 
+
+
+
+ 			$scope.saveChanges = function(){
+ 				var reff = new Firebase("https://thesportfinder.firebaseio.com/users/"+$scope.user.uid);
+ 				var reffObject = $firebaseObject(reff);
+ 				reffObject.$loaded().then(function(data){
+ 					console.log(data);
+ 					reffObject.description = $scope.user.description;
+ 				reffObject.$save().then(function(ref){
+ 					console.log(ref);
+ 				}, function(err){
+ 					console.log('error :', err);
+ 				})
+ 					console.log(reffObject);
+ 					$location.path('/TorT');
+				}).catch(function(err){console.log('Error: ', err)});
+			}
+		}
 	}]);
 
+	app.controller('TorTController', ['$scope', '$rootScope', '$firebaseObject', '$routeParams','$location', function($scope, $rootScope, $firebaseObject, $routeParams, $location){ 
+			if(!$scope.user){
+ 				$location.path('/');
+ 			}else{
 
+		$scope.profilePage = function(){
+			$location.path('/userProfile');
+		}
+	}
+	}]);
 
+	app.controller('searchDashboardController', ['$scope', '$rootScope', '$firebaseObject', '$routeParams','$location', function($scope, $rootScope, $firebaseObject, $routeParams, $location){ 
+		if(!$scope.user){
+ 				$location.path('/');
+ 			}else{
+
+ 			}
+	}]) 
 
 // =============================== SERVICES ====================================
